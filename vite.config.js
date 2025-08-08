df3b12d48a1a8373cb330dfc7002dfc19647cec7
Anushkaa2306 <bdm.lokendra@gmail.com>
2025-08-08 18:38:50 +0530

    Fix: Vite config for react-leaflet build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-
-export default {
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['react-leaflet', 'leaflet']
+  },
   build: {
     rollupOptions: {
-      external: ['react-leaflet', 'leaflet']
+      external: []
     }
   }
-}
+})
